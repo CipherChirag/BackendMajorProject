a64976534ae4d62e036cc5da508549cfd2c8c7e0
CipherChirag <chiragbhuwalka@gmail.com>
2025-02-11 11:01:14 +0530

    utils setup

--- backend/src/index.js
+++ backend/src/index.js
@@ -1,3 +1,12 @@
 import connectDB from './db/index.js';
+import { app } from './app.js';
 
 connectDB()
+.then(() => {
+    app.listen(process.env.PORT, () => {
+        console.log(`Server is running on port ${process.env.PORT}`);
+    });
+})
+.catch((error) => {
+    console.log('Error connecting to MongoDB: ', error);
+});
